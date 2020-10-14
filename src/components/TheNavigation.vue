dcdd4b6d1fcd08e029eaff7d7972f6272bdc9fa9
eligarlo <eligarlo.developer@gmail.com>
2020-10-14 19:40:14 +0300

    created navbar

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,27 @@
+<template>
+	<v-app-bar app color="primary" dark>
+		<router-link to="/" class="d-flex align-center">
+			<h2>My Task Board</h2>
+		</router-link>
+
+		<v-spacer></v-spacer>
+
+		<router-link to="/add">
+			<v-btn color="green darken-1" class="mr-2" rounded>
+				<span class="mr-2 d-none d-md-block">Add</span>
+				<v-icon>mdi-plus</v-icon>
+			</v-btn>
+		</router-link>
+
+		<router-link to="/settings">
+			<v-btn color="grey darken-2" rounded>
+				<span class="mr-2 d-none d-md-block">Settings</span>
+				<v-icon>mdi-cog</v-icon>
+			</v-btn>
+		</router-link>
+	</v-app-bar>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/components/TheNavigation.scss";
+</style>

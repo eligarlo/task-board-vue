7e5b9d7027614bae08583f90ee8ab0346dbeaeb9
eligarlo <eligarlo.developer@gmail.com>
2020-10-18 04:53:14 +0300

    create home view, display notes on loop

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <transition-group name="delete-post-it" tag="div" class="home">
+    <PostItNote v-for="note in notes" :key="note.id" :note="note" @delete-post-it="deletePostIt" />
+  </transition-group>
+</template>
+
+<script>
+// @ is an alias to /src
+import PostItNote from "@/components/PostItNote.vue"
+import Store from "@/utils/Store.js"
+
+export default {
+	name: "Home",
+	components: {
+		PostItNote,
+	},
+	data() {
+		return {
+			notes: Store.getPostIts()
+		}
+  },
+  methods: {
+    deletePostIt(id) {
+      Store.deletePostIt(id)
+      this.notes = Store.getPostIts()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/views/Home.scss";
+</style>
